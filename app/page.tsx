caf07392b3d5940f0255f274cbc263f91dc86dff
destinyoo <ismailabdelkader0@gmail.com>
2024-04-12 16:57:35 +0100

    ready for example

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-
     <div>
       <Head>
         <title>Abdelkader's Portfolio</title>
@@ -13,36 +12,46 @@ export default function Home() {
 
       {/* Header Menu */}
       <header className="bg-gray-900 text-white p-4">
-        {/* Your header menu items go here */}
-        <nav>
-          <ul className="flex justify-end">
-            <li className="mr-4"><a href="#" className="hover:text-gray-400">Home</a></li>
-            <li className="mr-4"><a href="#" className="hover:text-gray-400">About</a></li>
-            <li className="mr-4"><a href="#" className="hover:text-gray-400">Portfolio</a></li>
-            <li><a href="#" className="hover:text-gray-400">Contact</a></li>
-          </ul>
-        </nav>
+        {/* Logo */}
+        <div className="flex items-center justify-between">
+          {/* Replace "logo.png" with the path to your logo image */}
+          <img
+            className="h-10 w-auto"
+            src="/logo.png"
+            alt="Logo"
+          />
+
+          {/* Your header menu items go here */}
+          <nav>
+            <ul className="flex justify-end">
+              <li className="mr-4"><a href="#" className="hover:text-gray-400">Home</a></li>
+              <li className="mr-4"><a href="#" className="hover:text-gray-400">About</a></li>
+              <li className="mr-4"><a href="#" className="hover:text-gray-400">Portfolio</a></li>
+              <li><a href="#" className="hover:text-gray-400">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
       </header>
 
       {/* Profile Image and Text */}
       <main className="bg-gradient-to-t min-h-screen flex items-center justify-center">
         <div className="text-center">
-
           <Image
-            className="rounded-full h-40 w-40 mx-auto mb-4"
-            src="/profile.svg"
+            className="rounded-full h-20 w-20 mx-auto mb-4"
+            src="/profile.jpg"
             alt="Next.js Logo"
             width={180}
-            height={37}
+            height={180}
             priority
           />
 
-          <h1 className="text-4xl font-bold uppercase text-gray-600">Hi, I'm Abdelkader</h1>
-          <p className="text-lg text-gray-500 uppercase ">I'm a Full stack Developer & Cloud Engineer.</p>
+          <h1 className="text-sm font-bold uppercase text-gray-600">Hi, I'm Abdelkader</h1>
+          <p className="text-xs text-gray-500 uppercase">I'm a Full stack Developer🚀.</p>
         </div>
-        
       </main>
+      <footer className="bg-gray-900 text-white p-4 text-center">
+        <p>&copy; {new Date().getFullYear()} Abdelkader. All rights reserved.</p>
+      </footer>
     </div>
-
   );
 }
